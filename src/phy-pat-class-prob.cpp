9e538a24d3796db941624cdd541f423a6689f79a
Mark T. Holder <mtholder@gmail.com>
2011-07-10 15:02:57 -0500

    now appears to be calculating the parsimony score correctly

--- src/phy-pat-class-prob.cpp
+++ src/phy-pat-class-prob.cpp
@@ -19,33 +19,166 @@ std::string ConvertToBitFieldMatrix(const NxsCharactersBlock & cb, BitFieldMatri
 void writeBitFieldMatrix(std::ostream & out, const BitFieldMatrix & bitFieldMatrix);
 
 
+void calculatePatternClassProbabilities(const NxsSimpleTree & tree, std::ostream & out );
+void classifyObservedDataIntoClasses(const NxsSimpleTree & tree, const BitFieldMatrix &, std::ostream & out );
+
+
 ////////////////////////////////////////////////////////////////////////////////
 // cpp
 ////////////////////////////////////////////////////////////////////////////////
+std::vector<unsigned> gZeroVec;
 
-std::string ConvertToBitFieldMatrix(const NxsCharactersBlock & cb, BitFieldMatrix & mat, const NxsUnsignedSet * toInclude)
-    {
-    NxsString errormsg;
+void calculatePatternClassProbabilities(const NxsSimpleTree & tree, std::ostream & out ) {
+	std::vector<const NxsSimpleNode *> preorderVec = tree.GetPreorderTraversal();
+	for (std::vector<const NxsSimpleNode *>::const_reverse_iterator ndIt = preorderVec.rbegin();
+			ndIt != preorderVec.rend();
+			++ndIt) {
+		const NxsSimpleNode * nd = *ndIt;
+		std::vector<NxsSimpleNode *> children = nd->GetChildren();
+		out << "In calculatePatternClassProbabilities at node " << nd->GetTaxonIndex() << ", #children = " << children.size() << "\n";
+	}
+}
+
+class ParsInfo {
+	public:
+		ParsInfo()
+			:downPass(0),
+			allSeen(0),
+			score(0),
+			numPatterns(0) {
+			}
+		unsigned size() const {
+			return numPatterns;
+		}
+		void CalculateForTip(const BitFieldRow & data) {
+			assert(gZeroVec.size() >= data.size());
+			this->numPatterns = data.size();
+			this->downPass = &data[0];
+			this->allSeen = &data[0];
+			this->score = &gZeroVec[0];
+		}
+		void CalculateForInternal(const ParsInfo & leftData, const ParsInfo & rightData) {
+			this->numPatterns = leftData.size();
+			assert(numPatterns == rightData.size());
+			this->downPassOwned.resize(numPatterns);
+			this->downPass = &this->downPassOwned[0];
+			this->allSeenOwned.resize(numPatterns);
+			this->allSeen = &this->allSeenOwned[0];
+			this->scoreOwned.resize(numPatterns);
+			this->score = &this->scoreOwned[0];
+			const BitField * leftDown = leftData.downPass; 
+			const BitField * leftAll = leftData.allSeen; 
+			const unsigned * leftScore = leftData.score; 
+			const BitField * rightDown = rightData.downPass; 
+			const BitField * rightAll = rightData.allSeen; 
+			const unsigned * rightScore = rightData.score; 
+			for (unsigned i = 0; i < numPatterns; ++i, ++leftDown, ++leftAll, ++leftScore, ++rightDown, ++rightAll, ++rightScore) {
+				const BitField lrIntersection = (*leftDown) & (*rightDown);
+				const unsigned accumulScore = *leftScore + *rightScore;
+				this->allSeenOwned[i] = (*leftAll) | (*rightAll);
+				if (lrIntersection == BitField(0)) {
+					const BitField lrUnion = (*leftDown) | (*rightDown);
+					this->downPassOwned[i] = lrUnion;
+					this->scoreOwned[i] = 1 + accumulScore;
+				}
+				else {
+					this->downPassOwned[i] = lrIntersection;
+					this->scoreOwned[i] = accumulScore;
+				}
+			}
+		}
+
+		void write(std::ostream & o) const {
+			unsigned totalScore = 0;
+			for (unsigned i = 0; i < numPatterns; ++i) {
+				o << (int)score[i] << ' ' << (int)downPass[i] << ' ' << (int)allSeen[i] << '\n';
+				totalScore += score[i];
+			}
+			o << "totalScore = " << totalScore << '\n';
+		}
+		const BitField * downPass; // alias
+		const BitField * allSeen; // alias
+		const unsigned * score; // alias
+		
+	private:
+		unsigned numPatterns;
+		BitFieldRow downPassOwned;
+		BitFieldRow allSeenOwned;
+		std::vector<unsigned> scoreOwned;
+		
+};
+
+typedef std::pair<const NxsSimpleNode *, unsigned> NodeID;
+typedef std::map<NodeID, ParsInfo> NodeIDToParsInfo;
+
+void classifyObservedDataIntoClasses(const NxsSimpleTree & tree, const BitFieldMatrix &mat, std::ostream & out ) {
+	std::vector<const NxsSimpleNode *> preorderVec = tree.GetPreorderTraversal();
+	NodeIDToParsInfo nodeIDToParsInfo;
+	const ParsInfo * rootParsInfo = 0L;
+	gZeroVec.assign(mat[0].size(), 0);
+	for (std::vector<const NxsSimpleNode *>::const_reverse_iterator ndIt = preorderVec.rbegin();
+			ndIt != preorderVec.rend();
+			++ndIt) {
+		const NxsSimpleNode * nd = *ndIt;
+		std::vector<NxsSimpleNode *> children = nd->GetChildren();
+		const unsigned numChildren = children.size();
+		out << "In calculatePatternClassProbabilities at node " << nd->GetTaxonIndex() << ", #children = " << numChildren << "\n";
+		NodeID currNdId(nd, 0);
+		ParsInfo & currParsInfo = nodeIDToParsInfo[currNdId];
+		if (numChildren == 0) {
+			const unsigned taxInd = nd->GetTaxonIndex();
+			currParsInfo.CalculateForTip(mat.at(taxInd));
+		}
+		else {
+			if (numChildren == 1)
+				throw NxsException("Trees of degree 2 are not supported, yet\n");
+			const NxsSimpleNode * leftNd = children[0];
+			const NxsSimpleNode * rightNd = children[1];
+			NodeIDToParsInfo::const_iterator leftPIIt= nodeIDToParsInfo.find(NodeID(leftNd, 0));
+			assert(leftPIIt != nodeIDToParsInfo.end());
+			NodeIDToParsInfo::const_iterator rightPIIt= nodeIDToParsInfo.find(NodeID(rightNd, 0));
+			assert(rightPIIt != nodeIDToParsInfo.end());
+			currParsInfo.CalculateForInternal(leftPIIt->second, rightPIIt->second);
+			
+			if (numChildren > 2) {
+				if (nd != preorderVec[0] || numChildren > 3)
+					throw NxsException("Parsimony scoring on non-binary trees is not supported, yet\n");
+				NodeID lastNdId(nd, 1);
+				ParsInfo & lastParsInfo = nodeIDToParsInfo[currNdId];
+				const NxsSimpleNode * lastNd = children.at(2);
+				NodeIDToParsInfo::const_iterator lastPIIt= nodeIDToParsInfo.find(NodeID(lastNd, 0));
+				assert(lastPIIt != nodeIDToParsInfo.end());
+				lastParsInfo.CalculateForInternal(currParsInfo, lastPIIt->second);
+				rootParsInfo = &lastParsInfo;
+			}
+			else if (nd == preorderVec[0])
+				rootParsInfo = &currParsInfo;
+		}
+	}
+	assert(rootParsInfo != 0L);
+	rootParsInfo->write(std::cerr);
+}
+
+std::string ConvertToBitFieldMatrix(const NxsCharactersBlock & cb, BitFieldMatrix & mat, const NxsUnsignedSet * toInclude) {
+	NxsString errormsg;
 	std::vector<const NxsDiscreteDatatypeMapper *> mappers = cb.GetAllDatatypeMappers();
 	if (mappers.empty() || mappers[0] == NULL)
 		throw NxsException("no mappers");
 
-    if (mappers.size() != 1)
-        throw NxsException("Expecting an unmixed characters block, but found a matrix with datatype = mixed or a datatype with augmented symbols\n");
-    NxsUnsignedSet scratchSet;
-	if (toInclude == 0L)
-		{
+	if (mappers.size() != 1)
+		throw NxsException("Expecting an unmixed characters block, but found a matrix with datatype = mixed or a datatype with augmented symbols\n");
+	NxsUnsignedSet scratchSet;
+	if (toInclude == 0L) {
 		for (unsigned i = 0; i < cb.GetNChar(); ++i)
 			scratchSet.insert(i);
 		toInclude = & scratchSet;
-	 	}
+	 }
 	
-    std::set <const NxsDiscreteDatatypeMapper * > usedMappers;
-	for (NxsUnsignedSet::const_iterator indIt = toInclude->begin(); indIt != toInclude->end(); ++indIt)
-		{
+	std::set <const NxsDiscreteDatatypeMapper * > usedMappers;
+	for (NxsUnsignedSet::const_iterator indIt = toInclude->begin(); indIt != toInclude->end(); ++indIt) {
 		unsigned charIndex = *indIt;
 		usedMappers.insert(cb.GetDatatypeMapperForChar(charIndex));
-		}
+	}
 
 	if (usedMappers.size() > 1)
 		throw NxsException("too many mappers");
@@ -56,172 +189,151 @@ std::string ConvertToBitFieldMatrix(const NxsCharactersBlock & cb, BitFieldMatri
 	const NxsDiscreteDatatypeMapper & mapper = **usedMappers.begin();
 
 	NxsCharactersBlock::DataTypesEnum inDatatype = mapper.GetDatatype();
-    const unsigned nStates =  mapper.GetNumStates();
-    if (nStates > MAX_NUM_STATES)
-        throw NxsException("MAX_NUM_STATES exceeded.  Recompile with a larger datatype for BitField.\n");
-    
-    for (NxsDiscreteStateCell i = 0; i < (NxsDiscreteStateCell)nStates; ++i)
-        {
-        if (mapper.GetStateSetForCode(i).size() != 1)
-            throw NxsException("Expecting the first states to correspond to state sets with size 1");
-        }
+	const unsigned nStates =  mapper.GetNumStates();
+	if (nStates > MAX_NUM_STATES)
+		throw NxsException("MAX_NUM_STATES exceeded.  Recompile with a larger datatype for BitField.\n");
+	
+	for (NxsDiscreteStateCell i = 0; i < (NxsDiscreteStateCell)nStates; ++i) {
+		if (mapper.GetStateSetForCode(i).size() != 1)
+			throw NxsException("Expecting the first states to correspond to state sets with size 1");
+	}
 	const std::string fundamentalSymbols = mapper.GetSymbols();
-	if (fundamentalSymbols.length() != nStates)
-	    {
-	    errormsg << "Expecting the fundamental symbols (" << fundamentalSymbols << ") to have length " << nStates;
-	    throw NxsException(errormsg);
-        }
+	if (fundamentalSymbols.length() != nStates) {
+		errormsg << "Expecting the fundamental symbols (" << fundamentalSymbols << ") to have length " << nStates;
+		throw NxsException(errormsg);
+	}
 	NCL_ASSERT((int)NXS_MISSING_CODE < 0);
 	NCL_ASSERT((int)NXS_GAP_STATE_CODE < 0);
 
-    
-    const unsigned nTaxa = cb.GetNTax();
-    const unsigned includedNChar = toInclude->size();
-    mat.resize(nTaxa);
-    for (unsigned i = 0; i < nTaxa; ++i)
-        {
-        BitFieldRow & bfRow = mat[i];
-        bfRow.resize(includedNChar);
-    	const NxsDiscreteStateRow & row = cb.GetDiscreteMatrixRow(i);
-    	if (row.empty())
-		    {
-		    errormsg << "Empty row encountered for taxon " << (1+i) << ". Missing data is not supported.\n";
+	
+	const unsigned nTaxa = cb.GetNTax();
+	const unsigned includedNChar = toInclude->size();
+	mat.resize(nTaxa);
+	for (unsigned i = 0; i < nTaxa; ++i) {
+		BitFieldRow & bfRow = mat[i];
+		bfRow.resize(includedNChar);
+		const NxsDiscreteStateRow & row = cb.GetDiscreteMatrixRow(i);
+		if (row.empty()) {
+			errormsg << "Empty row encountered for taxon " << (1+i) << ". Missing data is not supported.\n";
 			throw NxsException(errormsg);
-		    }
-		unsigned j = 0; 
-		for (NxsUnsignedSet::const_iterator tIncIt = toInclude->begin(); tIncIt != toInclude->end(); ++tIncIt, ++j)
-		    {
-		    const NxsDiscreteStateCell & cell = row.at(*tIncIt);
-		    if (cell < 0 || cell >= (NxsDiscreteStateCell) nStates)
-		        {
-	    	    errormsg << "Ambiguous/missing/gap data found for taxon " << (1+i) << " at site " << *tIncIt << ". Missing data is not supported.\n";
-    			throw NxsException(errormsg);
-    			}
-    		const int bfi = 1 << (int) cell;
-    		bfRow[j] = BitField(bfi);
-		    }
 		}
+		unsigned j = 0; 
+		for (NxsUnsignedSet::const_iterator tIncIt = toInclude->begin(); tIncIt != toInclude->end(); ++tIncIt, ++j) {
+			const NxsDiscreteStateCell & cell = row.at(*tIncIt);
+			if (cell < 0 || cell >= (NxsDiscreteStateCell) nStates) {
+				errormsg << "Ambiguous/missing/gap data found for taxon " << (1+i) << " at site " << *tIncIt << ". Missing data is not supported.\n";
+				throw NxsException(errormsg);
+			}
+			const int bfi = 1 << (int) cell;
+			bfRow[j] = BitField(bfi);
+		}
+	}
 	return fundamentalSymbols;
-    }   
+}	
 
-void writeBitFieldMatrix(std::ostream & out, const BitFieldMatrix & bitFieldMatrix)
-    {
-    for (unsigned i = 0; i < bitFieldMatrix.size(); ++i)
-        {
-        out << "taxon " << (i + 1) << ":\n";
-        const BitFieldRow & row = bitFieldMatrix[i];
-        for (unsigned j = 0; j < row.size(); ++j)
-            out << ' ' << (int) row[j];
-        out << '\n';
-        }
-    }
-    
-int main(int argc, char * argv[])
-{
+void writeBitFieldMatrix(std::ostream & out, const BitFieldMatrix & bitFieldMatrix) {
+	for (unsigned i = 0; i < bitFieldMatrix.size(); ++i) {
+		out << "taxon " << (i + 1) << ":\n";
+		const BitFieldRow & row = bitFieldMatrix[i];
+		for (unsigned j = 0; j < row.size(); ++j)
+			out << ' ' << (int) row[j];
+		out << '\n';
+	}
+}
+	
+int main(int argc, char * argv[]) {
 	/* Usually it is easiest to surround interactions with NCL in a try block
 		that catches NxsException instances.
 		Reading files can certainly generate these errors, but even queries
 		after the parse can result in these exceptions.
 	*/
-	if (argc < 2)
-		{
+	if (argc < 2) {
 		std::cerr << "Expecting one arguments: a file name\n";
 		return 1;
-		}
-	if (argv[1][0] == '-' &&  argv[1][1] == 'h' && argv[1][2] == '\0' )
-		{
+	}
+	if (argv[1][0] == '-' &&  argv[1][1] == 'h' && argv[1][2] == '\0' ) {
 		std::cerr << "Takes a arguments: The path to a NEXUS file with a single characters block\n";
 		return 0;
-		}
+	}
 
 	std::string filename(argv[1]);
 	try {
-		int blocksToRead =  (PublicNexusReader::NEXUS_TAXA_BLOCK_BIT
+		int blocksToRead =	(PublicNexusReader::NEXUS_TAXA_BLOCK_BIT
 							| PublicNexusReader::NEXUS_CHARACTERS_BLOCK_BIT
 							| PublicNexusReader::NEXUS_ASSUMPTIONS_BLOCK_BIT
 							| PublicNexusReader::NEXUS_SETS_BLOCK_BIT
+							| PublicNexusReader::NEXUS_TREES_BLOCK_BIT
 							);
 		MultiFormatReader nexusReader(blocksToRead, NxsReader::WARNINGS_TO_STDERR);
-        
+		
 		std::cerr << "Reading " << filename << "\n";
 		try {
 			nexusReader.ReadFilepath(filename.c_str(), MultiFormatReader::NEXUS_FORMAT);
-			}
-		catch(const NxsException &x)
-			{
+		}
+		catch(const NxsException &x) {
 			std::cerr << "Error:\n " << x.msg << std::endl;
 			if (x.line > 0 || x.pos > 0)
 				std::cerr << "at line " << x.line << ", column (approximately) " << x.col << " (and file position "<< x.pos << ")" << std::endl;
 			return 2;
-			}
-		catch(...)
-			{
+		}
+		catch(...) {
 			nexusReader.DeleteBlocksFromFactories();
 			std::cerr << "Exiting with an unknown error" << std::endl;
 			return 1;
-			}
+		}
 
 		const unsigned numTaxaBlocks = nexusReader.GetNumTaxaBlocks();
-		if (numTaxaBlocks != 1)
-			{
+		if (numTaxaBlocks != 1) {
 			std::cerr << "Expecting a file with exactly 1 TAXA block, but found " << numTaxaBlocks << " in the file " << filename << ".\n";
 			return 2;
-			}
+		}
 		NxsTaxaBlock * taxaBlock = nexusReader.GetTaxaBlock(0);
 		const unsigned nCharsBlocks = nexusReader.GetNumCharactersBlocks(taxaBlock);
-		if (nCharsBlocks != 1)
-			{
+		if (nCharsBlocks != 1) {
 			std::cerr << "Expecting a file with exactly 1 CHARACTERS/DATA block, but found " << nCharsBlocks << " in the file " << filename << ".\n";
 			return 3;
-			}
-		NCL_COULD_BE_CONST  NxsCharactersBlock * charsBlock = nexusReader.GetCharactersBlock(taxaBlock, 0);
+		}
+		NCL_COULD_BE_CONST	NxsCharactersBlock * charsBlock = nexusReader.GetCharactersBlock(taxaBlock, 0);
 		assert(charsBlock);
 		BitFieldMatrix bitFieldMatrix;
 		std::string symbols = ConvertToBitFieldMatrix(*charsBlock, bitFieldMatrix);
 
-        writeBitFieldMatrix(std::cerr, bitFieldMatrix);
+		writeBitFieldMatrix(std::cerr, bitFieldMatrix);
 
 		const unsigned nTreesBlocks = nexusReader.GetNumTreesBlocks(taxaBlock);
-		if (nTreesBlocks == 0)
-			{
+		if (nTreesBlocks == 0) {
 			std::cerr << "Expecting a file with at least 1 TREES block.\n";
 			return 3;
-			}
+		}
 		unsigned totalTreeIndex = 0;
-		for (unsigned treesBlockInd = 0; treesBlockInd < nTreesBlocks; ++treesBlockInd) 
-		    {
-            NCL_COULD_BE_CONST  NxsTreesBlock * treesBlock = nexusReader.GetTreesBlock(taxaBlock, treesBlockInd);
-            assert(treesBlock);
-            treesBlock->ProcessAllTrees();
-            const unsigned nTreesThisBlock = treesBlock->GetNumTrees();
-            for (unsigned treeInd = 0; treeInd < nTreesThisBlock; ++treeInd)
-                {
-        		const NxsFullTreeDescription & ftd = treesBlock->GetFullTreeDescription(treeInd);
-        		if (ftd.AllEdgesHaveLengths())
-        		    {
-        		    NxsSimpleTree nclTree(ftd, 0, 0.0);
-        		    }
-        		else
-        		    {
-        		    std::cerr << "Tree " << (1 + treeInd) << " of TREES block " << (1 + treesBlockInd) << " does not lengths for all of the edges. Skipping this tree.\n";
-        		    }
-                    
-
-                }
-            }		
+		for (unsigned treesBlockInd = 0; treesBlockInd < nTreesBlocks; ++treesBlockInd) {
+			NCL_COULD_BE_CONST	NxsTreesBlock * treesBlock = nexusReader.GetTreesBlock(taxaBlock, treesBlockInd);
+			assert(treesBlock);
+			treesBlock->ProcessAllTrees();
+			const unsigned nTreesThisBlock = treesBlock->GetNumTrees();
+			for (unsigned treeInd = 0; treeInd < nTreesThisBlock; ++treeInd) {
+				const NxsFullTreeDescription & ftd = treesBlock->GetFullTreeDescription(treeInd);
+				if (ftd.AllEdgesHaveLengths()) {
+					NxsSimpleTree nclTree(ftd, 0, 0.0);
+					calculatePatternClassProbabilities(nclTree, std::cout);
+					classifyObservedDataIntoClasses(nclTree, bitFieldMatrix, std::cout);
+				}
+				else {
+					std::cerr << "Tree " << (1 + treeInd) << " of TREES block " << (1 + treesBlockInd) << " does not lengths for all of the edges. Skipping this tree.\n";
+				}
+			}
+		}		
 		nexusReader.DeleteBlocksFromFactories();
 	}
-	catch(const NxsException &x)
-		{
+	catch(const NxsException &x) {
 		std::cerr << "Error:\n " << x.msg << std::endl;
 		if (x.line > 0 || x.pos > 0)
 			std::cerr << "at line " << x.line << ", column (approximately) " << x.col << " (and file position "<< x.pos << ")" << std::endl;
 		return 2;
-		}
-	catch(...)
-		{
+	}
+	catch(...) {
 		std::cerr << "Exiting with an unknown error" << std::endl;
 		return 1;
-		}
+	}
 	return 0;
 }
