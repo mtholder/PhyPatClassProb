32dde392101e6302df7e9e70ea549027e6be46bf
Jordan Koch <jordankoch@Jordan-Kochs-MacBook.local>
2013-12-11 10:23:05 -0600

    DONE

--- test/our_prob.py
+++ test/our_prob.py
@@ -1,3 +1,4 @@
+import sys
 our_prob = dict()
 ourLikeFile = sys.argv[1]
 paupLikeFile = sys.argv[2]
@@ -12,25 +13,22 @@ for line in open(ourLikeFile, "rU"):
             our_prob[states] = our_prob[states]+prob
         else:
             our_prob[states] = prob
-        our_prob[states] = prob + our_prob.get(states,0.0)
+#        our_prob[states] = prob + our_prob.get(states,0.0)
 
 paup_prob = dict()
-for line in open(paupLikeFile, 'rU'): 
-    if not line.startswith('i'): 
+for line in open(paupLikeFile, 'rU'):
+    if line.strip() and not line.startswith('i'): #line.strip skips whitespace
         ls = line.split()
-        states = ls[5]
-        prob = float(ls[-1])
+        states = ls[1] #3 
+        prob = float(ls[2])
         paup_prob[states] = prob
-        
-        if states in paup_prob:
-            paup_prob[states] = prob
-        paup_prob[states] = 
 
-
-pk = paup_probs.keys()
+pk = paup_prob.keys()
 ok = our_prob.keys()
 pk.sort()
 ok.sort()
+print pk
+print ok
 assert pk == ok
 
 for k in pk:
