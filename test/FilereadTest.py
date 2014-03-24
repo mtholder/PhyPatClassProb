70aafb3fc4f2468dc7ede7b015d45ff51edafd60
Jordan Koch <jordankoch@Jordan-Kochs-MacBook.local>
2014-03-24 15:58:49 -0500

    new stuff

--- test/FilereadTest.py
+++ test/FilereadTest.py
@@ -3,15 +3,17 @@ likeFile = sys.argv[1]
 logFile = sys.argv[2]
 likeFileO = open(likeFile, 'rU')
 logFileO = open(logFile, 'rU')
-for line in logFileO: 
-    print line,
-    if line.startswith('Character        Type         Status      Weight   States'): 
-        break
-Uninformative = []
-logFileO.next()
-for line in logFileO: 
-    if line.strip()=='': 
-        break
-    x = line.split()
-    print x[2]
-    ind = int(x[0])
+def readLogFile(logFileO):
+    for line in logFileO: 
+        print line,
+        if line.startswith('Character        Type         Status      Weight   States'): 
+            break
+    Uninformative = []
+    logFileO.next()
+    for line in logFileO: 
+        if line.strip()=='': 
+            break
+        x = line.split()
+        print x[2]
+        ind = int(x[0])
+    return Uninformative
